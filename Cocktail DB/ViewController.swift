94d8fd8f33aed659069baae24e6d48be0f9f366f
Kostya Bershov <kbershov1@gmail.com>
2020-01-11 20:42:53 +0200

    Initial Commit

---
+++ Cocktail DB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cocktail DB
+//
+//  Created by Kostya Bershov on 11.01.2020.
+//  Copyright © 2020 Kostya Bershov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
