b16c159592a6cd6f726f524be134e1d61c854a01
Kostya Bershov <kbershov1@gmail.com>
2020-01-23 04:12:36 +0200

    Final Project edit

---
+++ Cocktail DB/View/CustomHeaderSection/CustomHeaderSectionView.swift
@@ -0,0 +1,23 @@
+//
+//  CustomHeaderSectionView.swift
+//  cocktail-db
+//
+//  Created by Kostya Bershov on 22.01.2020.
+//  Copyright © 2020 Kostya Bershov. All rights reserved.
+//
+
+import UIKit
+
+class CustomHeaderSectionView: UITableViewHeaderFooterView {
+
+    @IBOutlet weak var categoryLabel: UILabel!
+    
+    static func headerHeight() -> CGFloat {
+        return 30.0
+    }
+    
+    func setcategoryLabelText(text: String) {
+        categoryLabel.text = text
+    }
+
+}
